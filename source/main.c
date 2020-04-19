e11ad6ff06e8d8aeccafa208ac0cfbf6caaead74
Nichole Mattera <697668+NicholeMattera@users.noreply.github.com>
2020-04-19 02:50:02 -0400

    :sparkles: Use stat to check if files exists, and read directory listings to delete all files.

--- source/main.c
+++ source/main.c
@@ -1,10 +1,14 @@
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <switch.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 u32 __nx_applet_type = AppletType_None;
 
-#define INNER_HEAP_SIZE 0x1000
+#define INNER_HEAP_SIZE 0x9000
 size_t nx_inner_heap_size = INNER_HEAP_SIZE;
 char   nx_inner_heap[INNER_HEAP_SIZE];
 
@@ -41,14 +45,9 @@ void __attribute__((weak)) __appExit(void) {
     smExit();
 }
 
-bool fileExists(char * path) {
-    FILE * file = fopen(path, "r");
-    if (file) {
-        fclose(file);
-        return true;
-    }
-
-    return false;
+bool fileExists(const char * file) {
+    struct stat buf;
+    return (stat(file, &buf) == 0);
 }
 
 void deleteFile(char * path) {
@@ -57,28 +56,38 @@ void deleteFile(char * path) {
     }
 }
 
+void deleteFolder(char * path) {
+    struct dirent * de;
+    DIR * dir = opendir(path); 
+  
+    if (dir == NULL)
+        return; 
+  
+    while ((de = readdir(dir)) != NULL) {
+        if (de->d_type == DT_REG) {
+            char * fullpath;
+            fullpath = malloc(strlen(path) + strlen(de->d_name) + 2);
+            strcpy(fullpath, path);
+            strcat(fullpath, "/");
+            strcat(fullpath, de->d_name);
+
+            deleteFile(fullpath);
+
+            free(fullpath);
+        }
+    }
+  
+    closedir(dir);
+    rmdir(path);
+}
+
 int main(int argc, char * argv[]) {
     // Old Tesla
-    deleteFile("sdmc:/atmosphere/contents/010000000007E51A/flags/boot2.flag");
-    rmdir("sdmc:/atmosphere/contents/010000000007E51A/flags");
-    deleteFile("sdmc:/atmosphere/contents/010000000007E51A/exefs.nsp");
-    deleteFile("sdmc:/atmosphere/contents/010000000007E51A/toolbox.json");
-    rmdir("sdmc:/atmosphere/contents/010000000007E51A");
+    deleteFolder("sdmc:/atmosphere/contents/010000000007E51A/flags");
+    deleteFolder("sdmc:/atmosphere/contents/010000000007E51A");
 
     // Nichole Logo
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/01890C643E9D6E17B2CDA77A9749ECB9A4F676D6000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/0767302E1881700608344A3859BC57013150A375000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/3EC573CB22744A993DFE281701E9CBFE66C03ABD000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/723DF02F6955D903DF7134105A16D48F06012DB1000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/7421EC6021AC73DD60A635BC2B3AD6FCAE2A6481000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/7B4123290DE2A6F52DE4AB72BEA1A83D11214C71000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/7C5894688EDA24907BC9CE7013630F365B366E4A000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/967F4C3DFC7B165E4F7981373EC1798ACA234A45000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/98446A07BC664573F1578F3745C928D05AB73349000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/C088ADC91417EBAE6ADBDF3E47946858CAFE1A82000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/C79F22F18169FCD3B3698A881394F6240385CDB1000000000000000000000000.ips");
-    deleteFile("sdmc:/atmosphere/exefs_patches/nichole_logo/96529C3226BEE906EE651754C33FE3E24ECAE832000000000000000000000000.ips");
-    rmdir("sdmc:/atmosphere/exefs_patches/nichole_logo");
+    deleteFolder("sdmc:/atmosphere/exefs_patches/nichole_logo");
 
     // Prevent this from running again.
     deleteFile("sdmc:/atmosphere/contents/010000000000DA7A/flags/boot2.flag");
